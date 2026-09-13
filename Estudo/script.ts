104cb3fbf8c3032fd2d77986a8121132fc7f9201
devgustavoaraujo <gustavosantanaaraujo55@gmail.com>
2026-09-13 20:40:00 -0300

    Atualizando Estudos

--- Estudo/script.ts
+++ Estudo/script.ts
@@ -113,61 +113,65 @@
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
 
   // => Exemplo 01 - Uso Simples de Tuplas em Typescript
-  let pessoa2: [string, string, number] = ["Gustavo", "SP", 21]
-  pessoa2 = ["Gustavo", "São Paulo", 180] 
+  let pessoa2: [string, string, number] = ["Gustavo", "SP", 21];
+  pessoa2 = ["Gustavo", "São Paulo", 180];
   //console.log(pessoa2)
 
   // => Exemplo 02 - Acessando o valor da Tupla
-  let pessoa3: [string, string, number]
-  pessoa3 = ["Gustavo", "São Paulo", 180]
+  let pessoa3: [string, string, number];
+  pessoa3 = ["Gustavo", "São Paulo", 180];
   //console.log(pessoa3[0])
 
   // => Exemplo 03 - Outra forma de usar tuplas em Typescript (com Labels)
-  
-  let dadosPessoa: [nome: string, posicao: string, idade: number] = ["Gustavo", "São Paulo", 180]
+
+  let dadosPessoa: [nome: string, posicao: string, idade: number] = [
+    "Gustavo",
+    "São Paulo",
+    180,
+  ];
   //console.log(dadosPessoa)
 
   //Exemplo 04 - Usando Tuplas com Spread Operator
 
-  let listaFrutas: [string, ...string[]] = ["Maça", "Banana", "Uva", "Laranja"]  
+  let listaFrutas: [string, ...string[]] = ["Maça", "Banana", "Uva", "Laranja"];
   //console.log(...listaFrutas)
-  
+
   //EXISTEM DIVERSOS TIPOS DE FRUTAS, USA ...STRING PARA QUANDO NÃO SABE A QUANTIDADE
   //..STRING JÁ INDICA QUE E UMA SEQUÊNCIA DE STRINGS
 
   // => Exemplo 05 - Lista Heterogênea de Tupla
 
-  let totalFrutas: [number, boolean, ...string[]] = [5, true, ...listaFrutas]
+  let totalFrutas: [number, boolean, ...string[]] = [5, true, ...listaFrutas];
   //console.log(totalFrutas)
 
   // => Exemplo 06 - Uso de função com Tuplas
 
-  function listarPessoas(nomes: string[], idades: number[]){
-    return [...nomes, ...idades]
+  function listarPessoas(nomes: string[], idades: number[]) {
+    return [...nomes, ...idades];
   }
-  let resultado = listarPessoas(["Gustavo", "Matheus"], [32, 22])
+  let resultado = listarPessoas(["Gustavo", "Matheus"], [32, 22]);
   //console.log(resultado)
 
   // => Exemplo 07 - Labeled Tuples com Spread Operator em uma Função
   type Nome =
-        | [primeiroNome: string, sobrenome: string] 
-        | [primeiroNome: string, nomeMeio: string, sobrenome: string]
+    | [primeiroNome: string, sobrenome: string]
+    | [primeiroNome: string, nomeMeio: string, sobrenome: string];
 
-  function criarPessoa(...nome: Nome){
-    return [...nome]
-  }      
+  function criarPessoa(...nome: Nome) {
+    return [...nome];
+  }
   //console.log(criarPessoa("Gustavo", "Santana", "Araujo"))
 
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
   // -==-==-==-==                       => Tipo Enum <=                ==-==-==-==-==-==-==-==-==-==- //
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
-  
+
   //ENUM TIPO NÚMERICO (RECOMENDAVEL)
   enum Idiomas {
     Portugues,
     Espanhol,
     Ingles,
-    Frances
+    Frances,
   }
   //console.log(Idiomas)
 
@@ -179,7 +183,7 @@
     Quinta = "Quin",
     Sexta = "Sex",
     Sabado = "Sab",
-    Domingo = "Dom"
+    Domingo = "Dom",
   }
   //console.log(Dias)
   //console.log(Dias.Segunda)
@@ -191,55 +195,55 @@
     Massa,
     Pizza,
     Sushi,
-    Churrasco
+    Churrasco,
   }
 
-  function comida(c: Comida){
-    return "Comidas muito Apetitosas!"
+  function comida(c: Comida) {
+    return "Comidas muito Apetitosas!";
   }
   //console.log(comida(Comida.Pizza))
 
   // => Exemplo 03: Quando usar o Enum?
 
-  enum Tarefa{
+  enum Tarefa {
     Todo,
     Progress,
-    Done
+    Done,
   }
   const concluidaTarefa = {
     id: 1,
     status: Tarefa.Done,
-    descricao: "Parabéns, Tarefa Concluida com Sucesso!"
-  }
+    descricao: "Parabéns, Tarefa Concluida com Sucesso!",
+  };
 
-  if(concluidaTarefa.status === Tarefa.Done) {
-      //console.log("Enviar E-mail: Tarefa Concluída!")
+  if (concluidaTarefa.status === Tarefa.Done) {
+    //console.log("Enviar E-mail: Tarefa Concluída!")
   }
 
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
   // -==-==-==-==                       => Tipo Any <=                  ==-==-==-==-==-==-==-==-==-==- //
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
-  
+
   const a: any = 888;
-  const b: any = ["Gustavo"]
-  const result = a + b
+  const b: any = ["Gustavo"];
+  const result = a + b;
   //console.log(result)
-  
+
   // => Exemplo 02: Quando o Tipo ANY é Inferido Implicitamente
 
   let frase;
-  frase = "Olá Mundo"
+  frase = "Olá Mundo";
   //console.log(frase)
 
   // => Exemplo 03: Quando devemos usar o Tipo Any?
 
-  const formulario:{[campoFormulario: string]: any} = {
+  const formulario: { [campoFormulario: string]: any } = {
     nome: "Gustavo",
     Sobrenome: "Santana",
-    Idade: "21"
-  }
+    Idade: "21",
+  };
   //console.log(formulario)
-  
+
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
   // -==-==-==-==                       => Tipo Unknown <=              ==-==-==-==-==-==-==-==-==-==- //
   // -==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==-==- //
@@ -255,8 +259,21 @@
   valorVariavel = true; //SEM ERRO
   valorVariavel = 123; //SEM ERRO
   valorVariavel = []; //SEM ERRO
-  valorVariavel = "Olá, tudo bem?" //SEM ERRO
+  valorVariavel = "Olá, tudo bem?"; //SEM ERRO
 
-  
+  //Exemplo 02: Erro Tentar Atribuir Valor do Tipo Unknown em outros Tipos!
+
+  //Exemplo 03: => Diferença entre "Any" e o Unknown
+
+  let algumacoisaAny: any;
+  let algumacoisaUnknown: unknown;
+
+  //TO FIXED (FORMATA PARA DUAS CASAS DECIMAIS, OU PARA UMA CASA DECIMAL)
+
+  console.log(algumacoisaAny.toFixed(2)); //ERRO
+  //console.log(algumacoisaUnknown.toFixed(2)); //FALTA DECLARAR, RESOLVE USANDO UMA VERIFICAÇÃO
+
+  if (typeof algumacoisaUnknown === "number") {
+    console.log(algumacoisaUnknown.toFixed(2)); //FORÇA FAZER A VERIFICAÇÃO PARA USAR TOFIXED
+  }
 }
-
--- src/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//MODULO 1 INTRODUÇÃO AO TYPESCRIPT
-{
-    const mensagem = "Hello World";
-    console.log(mensagem);
-}
-//# sourceMappingURL=script.js.map
