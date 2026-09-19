cbbd6283071e21dcd075f1036211d85e8638362e
devgustavoaraujo <gustavosantanaaraujo55@gmail.com>
2026-09-19 09:15:51 -0300

    RGB

---
+++ ReactJS/rgb/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  //https://www.youtube.com/watch?v=BAVWdufqMe0&list=PLz4HUyGp2IylMffwmaBFu2O6fzT0BI4ya&index=19
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
