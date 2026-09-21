0d6487d7bcd44b885955fe25c9b2d13da33e4c81
devgustavoaraujo <gustavosantanaaraujo55@gmail.com>
2026-09-21 20:49:53 -0300

    Atualizando Estudos

---
+++ Javascript/script.js
@@ -0,0 +1,30 @@
+{
+  //alert("HelloWorld");
+  //console.log("HelloWorld");
+}
+
+//VARIÁVEIS E TIPOS DE DADOS
+
+{
+  const variavelConst = "Não é Alterável!";
+  const numberInt = 30;
+  let variavelLet = "Variável Alterável";
+
+  console.log(variavelConst);
+  console.log(numberInt);
+  console.log(variavelLet);
+
+  /*TIPOS DE DADOS*/
+
+  const textosStrings = "Hello World";
+  const stringCrase = `
+  Olá Mundo para Baixo
+  Com Crase e Possível
+  Escrever para Baixo
+  Não somente em uma
+  Linha!
+  ${textosStrings}
+  `;
+
+  console.log(stringCrase);
+}
