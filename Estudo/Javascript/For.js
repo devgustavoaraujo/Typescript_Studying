2f70b542cb9d2518ebc6ad770ad76cd90330a1e9
devgustavoaraujo <gustavosantanaaraujo55@gmail.com>
2026-09-23 17:30:16 -0300

    Atualizando Estudos

--- Estudo/Javascript/For.js
+++ Estudo/Javascript/For.js
@@ -12,19 +12,23 @@ const arraySimples = ["Gustavo", "Eduardo", "Gabriel", "João", "Eduardo"];
 
 for (let i = 0; i < 10; i++) {
   if (i <= arraySimples.length - 1) {
-    console.log(` ${i + 1}. ${arraySimples[i]}`);
+    //console.log(` ${i + 1}. ${arraySimples[i]}`);
   }
 }
+//console.log("Fim da Lista");
 
-console.log("Fim da Lista");
+const users = ["Gustavo", "Ana", "Joana", "Maria"];
 
+for (let i = 0; i < users.length; i++) {
+  console.log(`${i + 1}. ${users[i]}`);
+}
 setTimeout(() => {
-  console.log("Macaco");
+  //console.log("Macaco");
 }, 3000);
 
 for (let i = 0; i < 3; i++) {
   setTimeout((i) => {
     i = "macaco";
-    console.log(i);
+    //console.log(i);
   }, 3000);
 }
