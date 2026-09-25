ffcda63af80469a5c7f659521cfe14b2017c77ea
devgustavoaraujo <gustavosantanaaraujo55@gmail.com>
2026-09-25 09:23:58 -0300

    Atualizando Estudos do Switch Case

--- Estudo/Javascript/FuncoesSimples/Switch.js
+++ Estudo/Javascript/FuncoesSimples/Switch.js
@@ -27,7 +27,7 @@ switch (animal) {
   }
 }
 
-const stuff = "hamburguer";
+const stuff = "queijo";
 
 switch (stuff) {
   case "gato":
@@ -43,4 +43,52 @@ switch (stuff) {
     console.log("Comida!");
     break;
   }
+  default: {
+    console.log("Não Listado na Variavel");
+  }
 }
+
+const r1 = require("node:readline");
+const prompt = r1.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log("Bem vindo ao programa");
+console.log("[1] 📅 Data atual");
+console.log("[2] 🕒 Horário atual");
+console.log("[3] 🦓 Ver animais");
+console.log("[4] 🍕 Ver comidas");
+console.log("[5] 💻 Ver Linguagens");
+console.log("[0] ❌ Sair");
+
+prompt.question("> Selecione o que desejar fazer", (answer) => {
+  switch (answer) {
+    case "1": {
+      const date = new Date();
+      const day = date.getDay();
+      const month = date.getMonth() + 1; //JAVASCRIPT AS COISAS COMEÇAM EM 0, NÃO EXISTE MÊS 0 ENTÃO COLOCA +1
+      const year = date.getFullYear();
+      console.log(`Hoje é dia ${day}, do mês de ${month} no ano de ${year}`);
+      break;
+    }
+    case "2": {
+      const hours = new Date();
+      const hour = hours.getHours();
+      const minutes = hours.getMinutes();
+      const seconds = hours.getSeconds();
+      console.log(`Agora são ${hour}h ${minutes}m ${seconds} `);
+      break;
+    }
+    case "3": {
+      break;
+    }
+    case "4": {
+      break;
+    }
+    case "5": {
+      break;
+    }
+  }
+  prompt.close();
+});
